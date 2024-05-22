eb30fdac5d91d95356b1ee6b28d56e684e9cf06a
Mikołaj "Mitos" Mijakowski <fenix.warld@gmail.com>
2024-05-22 15:00:00 +0200

    Make form work (almost).

--- snp/forms.py
+++ snp/forms.py
@@ -5,5 +5,8 @@ from .models import Animal, SNP, Chromosome
 class SNPForm(forms.Form):
     animal = forms.IntegerField(label="Animal", widget=forms.HiddenInput())
     region = forms.CharField(label="Region")
+    region.widget.attrs.update({"class": "form-control"})
     maf_min = forms.FloatField(label="Min. value", min_value=0.0, max_value=0.5)
+    maf_min.widget.attrs.update({"class": "form-control"})
     maf_max = forms.FloatField(label="Max. value", min_value=0.0, max_value=0.5)
+    maf_max.widget.attrs.update({"class": "form-control"})
