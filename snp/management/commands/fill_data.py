13e86e1cce6a100d419bbb4d9746d9fefb64c63f
Mikołaj "Mitos" Mijakowski <fenix.warld@gmail.com>
2024-06-05 14:44:57 +0200

    Finish managment test data fill command.

--- snp/management/commands/fill_data.py
+++ snp/management/commands/fill_data.py
@@ -7,14 +7,17 @@ from snp.models import Animal, Chromosome, SNP, Annotation
 
 def create_annotations(snp):
     for i in range(randrange(5)):
-        a = Annotation(snp=snp, text="Lorem ipsum", author=choice(User.objects.all()))
+        a = Annotation(snp=snp, text="Lorem ipsum dolor sit amet.", author=choice(User.objects.all()))
         a.save()
 
 
 def create_snp(chromosome):
-    s = SNP(chromosome=chromosome, position=randrange(1, 1000), 
-    ref_allele=choice(['A', 'C', 'G', 'T']), alt_allele=choice(['A', 'C', 'G', 'T']),
-    maf=uniform(0.0, 0.5))
+    alleles = ['A', 'C', 'G', 'T']
+    allele = choice(alleles)
+    alleles.remove(allele)
+    s = SNP(chromosome=chromosome, position=randrange(1, 1000),
+            ref_allele=allele, alt_allele=choice(alleles),
+            maf=uniform(0.0, 0.5))
     s.save()
     create_annotations(s)
 
@@ -28,30 +31,32 @@ def create_chromosomes(animal, i):
 
 def create_animals():
     animals = [
-        {"name": "Daniel", "latin": "Dama dama", "image": "img/Dama_dama8.JPG"},
+        {"name": "Daniel", "latin": "Dama dama", "image": "img/daniel.jpg"},
         {"name": "Jeleń", "latin": "Cervus elaphus", "image": "img/jelen.jpg"},
+        {"name": "Lew", "latin": "Panthera leo", "image": "img/lew.jpg"},
+        {"name": "Pingwin", "latin": "Aptenodytes forsteri", "image": "img/pingwin.jpg"},
     ]
     for animal in animals:
         chromosomes = randrange(5, 45)
-        a = Animal(name=animal["name"], latin_name=animal["latin"], 
-                    image=UploadedFile(file=open(animal["image"], "rb")), chromosome_count=chromosomes)
+        a = Animal(name=animal["name"], latin_name=animal["latin"],
+                   image=UploadedFile(file=open(animal["image"], "rb")), chromosome_count=chromosomes)
         a.save()
         for i in range(chromosomes):
-            create_chromosomes(a, i+1)
+            create_chromosomes(a, i + 1)
 
 
 class Command(BaseCommand):
     help = "Creates dummy data to be used in database"
 
     # def add_arguments(self, parser):
-        # parser.add_argument("animal_list")
-        # parser.add_argument("animal_count", type=int)
-        # parser.add_argument("chromosome_min_count", type=int)
-        # parser.add_argument("chromosome_max_count", type=int)
-        # parser.add_argument("snp_min_count", type=int)
-        # parser.add_argument("snp_max_count", type=int)
-        # parser.add_argument("annotation_min_count", type=int)
-        # parser.add_argument("annotation_max_count", type=int)
+    # parser.add_argument("animal_list")
+    # parser.add_argument("animal_count", type=int)
+    # parser.add_argument("chromosome_min_count", type=int)
+    # parser.add_argument("chromosome_max_count", type=int)
+    # parser.add_argument("snp_min_count", type=int)
+    # parser.add_argument("snp_max_count", type=int)
+    # parser.add_argument("annotation_min_count", type=int)
+    # parser.add_argument("annotation_max_count", type=int)
 
     def handle(self, *args, **options):
         create_animals()
